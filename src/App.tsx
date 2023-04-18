cf17c10d0cad6c2fcb4e068180595f9a3f3704ea
den4592 <den4592@naver.com>
2023-04-18 16:15:02 +0900

    dev vite+ts migration

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./index.scss";
+
+import { BrowserRouter, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      hello
+      <BrowserRouter>
+        <Routes></Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
--- src/Rights.js
+++
@@ -1,9 +0,0 @@
-export const Rights = () => {
-  return (
-    <footer className="rights">
-      <hr />
-      <p className="contact">Contact : den4592@naver.com</p>
-      <p className="rights-text">©MemoryStrike 2022. All Rights Reserved.</p>
-    </footer>
-  );
-};
