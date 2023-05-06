f7de5e8a629892c4a331669026200265dfedc705
den4592 <den4592@naver.com>
2023-05-06 20:37:08 +0900

    content update 확인

--- client/src/pages/Memory/components/ContentCard/index.tsx
+++ client/src/pages/Memory/components/ContentCard/index.tsx
@@ -1,7 +1,8 @@
-import { memo } from "react";
+import { useState, useRef, memo } from "react";
 import axios from "axios";
 import "./index.scss";
 import { Link } from "react-router-dom";
+import EditContent from "../../../../assets/svgs/edit.svg";
 import Calendar from "../../../../assets/svgs/calendar.svg";
 import DeleteContent from "../../../../assets/svgs/remove.svg";
 import ArrowToBracket from "../../../../assets/svgs/arrow-right-to-bracket-solid.svg";
@@ -24,27 +25,70 @@ const ContentCard = ({
   setUpdateContents,
 }: ContentCardProps) => {
   let userId = window.localStorage.getItem("token");
+  const [enableEdit, setEnableEdit] = useState<boolean>(false);
+  const [contentText, setContentText] = useState<string>(content);
+  const [descriptionText, setDescriptionText] = useState<string>(description);
 
   const date = new Date(time).toLocaleString("ko-KR");
 
   const handleDelete = async (contentId: string) => {
     await axios.delete(`http://localhost:8080/api/contents/${contentId}`, {
       data: {
-        contentId,
+        id,
         userId,
       },
     });
     setUpdateContents(!updateContents);
   };
 
+  const handleEdit = () => {
+    if (enableEdit === true) {
+      axios
+        .put(`http://localhost:8080/api/contents/${id}`, {
+          content: contentText,
+          description: descriptionText,
+          creator: userId,
+        })
+        .then((res) => console.log(res));
+    }
+    setEnableEdit(!enableEdit);
+    setUpdateContents(!updateContents);
+  };
+
   return (
     <div className="content-card">
-      <p className="content-card-topic-label content-card-text">토픽</p>
-      <p className="content-card-topic-text ">{content}</p>
-      <p className="content-card-description-label content-card-text">설명</p>
-      <p className="content-card-description-text ">
-        {description ? description : "-"}
-      </p>
+      <p className="content-card-label">토픽</p>
+      <div className="content-card-text">
+        <p>
+          {enableEdit ? (
+            <input
+              type="text"
+              value={contentText}
+              className="content-card-text-input"
+              onChange={(e) => setContentText(e.target.value)}
+            />
+          ) : (
+            content
+          )}
+        </p>
+      </div>
+      <p className="content-card-description-label">설명</p>
+      <div className="content-card-description-text  content-card-text">
+        <p>
+          {enableEdit ? (
+            <input
+              type="text"
+              value={descriptionText}
+              className="content-card-text-input"
+              onChange={(e) => setDescriptionText(e.target.value)}
+            />
+          ) : description ? (
+            description
+          ) : (
+            "-"
+          )}
+        </p>
+      </div>
       <div className="content-card-date">
         <div className="content-card-date-calendar">
           <Calendar />
@@ -52,11 +96,22 @@ const ContentCard = ({
         <p className="content-card-date-text">{date}</p>
       </div>
       <div className="content-card-options">
+        {enableEdit ? (
+          <button className="content-card-options-confirm" onClick={handleEdit}>
+            확인
+          </button>
+        ) : (
+          ""
+        )}
+
         <div
           className="content-card-options-delete"
           onClick={() => handleDelete(id)}
         >
           <DeleteContent />
+        </div>
+        <div className="content-card-options-edit" onClick={handleEdit}>
+          <EditContent />
         </div>
         <Link
           to={{
