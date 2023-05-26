01602c44fd3e866d3e072f9ee1acdb0613e0ca13
den4592 <den4592@naver.com>
2023-05-26 15:03:34 +0900

    테이블 체크박스 추가

--- client/src/pages/Statistics/components/StatisticsTable/index.tsx
+++ client/src/pages/Statistics/components/StatisticsTable/index.tsx
@@ -1,4 +1,5 @@
-import { useSortBy, useTable } from "react-table";
+import { useSortBy, useTable, useRowSelect } from "react-table";
+import React, { forwardRef, useEffect } from "react";
 import "./index.scss";
 
 interface StatisticsTableProps {
@@ -6,9 +7,88 @@ interface StatisticsTableProps {
   data: any;
 }
 
+interface Props {
+  indeterminate?: boolean;
+  name: string;
+}
+
+const useCombinedRefs = (
+  ...refs: React.Ref<HTMLInputElement>[]
+): React.MutableRefObject<any> => {
+  const targetRef = React.useRef();
+
+  useEffect(() => {
+    refs.forEach((ref: any) => {
+      if (!ref) return;
+
+      if (typeof ref === "function") {
+        ref(targetRef.current!);
+      } else {
+        ref.current = targetRef.current;
+      }
+    });
+  }, [refs]);
+
+  return targetRef;
+};
+
+const IndeterminateCheckbox = forwardRef<HTMLInputElement, Props>(
+  ({ indeterminate, ...rest }, ref: React.Ref<HTMLInputElement>) => {
+    const defaultRef = React.useRef(null);
+    const combinedRef = useCombinedRefs(ref, defaultRef);
+
+    useEffect(() => {
+      if (combinedRef?.current) {
+        combinedRef.current.indeterminate = indeterminate ?? false;
+      }
+    }, [combinedRef, indeterminate]);
+
+    return (
+      <React.Fragment>
+        <input type="checkbox" ref={combinedRef} {...rest} />
+      </React.Fragment>
+    );
+  }
+);
+
 const StatisticsTable = ({ columns, data }: StatisticsTableProps) => {
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({ columns, data }, useSortBy);
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    selectedFlatRows,
+    state: { selectedRowIds },
+  } = useTable({ columns, data }, useSortBy, useRowSelect, (hooks) => {
+    hooks.visibleColumns.push((columns) => [
+      // Let's make a column for selection
+      {
+        id: "selection",
+        // The header can use the table's getToggleAllRowsSelectedProps method
+        // to render a checkbox
+        Header: ({ getToggleAllRowsSelectedProps }) => (
+          <div>
+            <IndeterminateCheckbox
+              name={""}
+              {...getToggleAllRowsSelectedProps()}
+            />
+          </div>
+        ),
+        // The cell can use the individual row's getToggleRowSelectedProps method
+        // to the render a checkbox
+        Cell: ({ row }) => (
+          <div>
+            <IndeterminateCheckbox
+              name={""}
+              {...row.getToggleRowSelectedProps()}
+            />
+          </div>
+        ),
+      },
+      ...columns,
+    ]);
+  });
 
   return (
     <>
@@ -64,6 +144,21 @@ const StatisticsTable = ({ columns, data }: StatisticsTableProps) => {
           </tbody>
         </table>
       </div>
+      <p>Selected Rows: {Object.keys(selectedRowIds).length}</p>
+      <pre>
+        <code>
+          {JSON.stringify(
+            {
+              selectedRowIds: selectedRowIds,
+              "selectedFlatRows[].original": selectedFlatRows.map(
+                (d) => d.original
+              ),
+            },
+            null,
+            2
+          )}
+        </code>
+      </pre>
     </>
   );
 };
