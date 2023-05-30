af07caaad823c6e031df893da1193f5457c79078
den4592 <den4592@naver.com>
2023-05-30 16:44:14 +0900

    메모리 페이지 로더 추가

--- client/src/api/content/getContents.ts
+++ client/src/api/content/getContents.ts
@@ -1,10 +1,10 @@
 import { httpClient } from "../index";
 
-export const getTopics = async (contentId: any) => {
+export const getContents = async (userId: any) => {
   let response;
   try {
     response = await httpClient.get(
-      `http://localhost:8080/api/topics/${contentId}`
+      `http://localhost:8080/api/contents/user/${userId}`
     );
   } catch (error) {}
   return response;
--- client/src/pages/Memory/index.tsx
+++ client/src/pages/Memory/index.tsx
@@ -3,11 +3,13 @@ import { useState, useEffect } from "react";
 import CreateContent from "./components/CreateContent";
 import axios from "axios";
 import ContentCard from "../Memory/components/ContentCard";
+import { getContents } from "../../api/content/getContents";
 
 const Memory = () => {
   const [toggle, setToggle] = useState<boolean>(false);
   const [updateContents, setUpdateContents] = useState<boolean>(false);
   const [contents, setContents] = useState([]);
+  const [loader, setLoader] = useState<boolean>(false);
 
   const createContentFromDefault = () => {
     window.scrollTo(0, 0);
@@ -16,53 +18,66 @@ const Memory = () => {
 
   const userData = JSON.parse(localStorage.getItem("userData")!);
 
+  const fetchContents = async () => {
+    try {
+      setLoader(true);
+      const getContentsResponse = await getContents(userData.userId);
+      if (getContentsResponse?.status === 200) {
+        setContents(getContentsResponse.data.contents);
+      }
+      setLoader(false);
+    } catch (error) {}
+  };
+
   useEffect(() => {
-    axios
-      .get(`http://localhost:8080/api/contents/user/${userData.userId}`)
-      .then((res) => {
-        setContents(res.data.contents);
-      });
+    fetchContents();
   }, [updateContents, userData.userId]);
 
   return (
     <div className="memory">
-      <div className="memory-utils">
-        <p className="memory-utils-category-length">
-          총 카테고리 : {contents.length}
-        </p>
-        <button onClick={() => setToggle(!toggle)} className="btn">
-          카테고리 추가
-        </button>
-      </div>
+      {loader ? (
+        <span className="loader"></span>
+      ) : (
+        <>
+          <div className="memory-utils">
+            <p className="memory-utils-category-length">
+              총 카테고리 : {contents.length}
+            </p>
+            <button onClick={() => setToggle(!toggle)} className="btn">
+              카테고리 추가
+            </button>
+          </div>
 
-      {toggle && (
-        <CreateContent
-          updateContents={updateContents}
-          setUpdateContents={setUpdateContents}
-        />
-      )}
-
-      <div className="memory-contents">
-        {contents?.map((content: any) => {
-          return (
-            <ContentCard
-              id={content._id}
-              key={content._id}
-              content={content.content}
-              description={content.description}
-              time={content.updatedAt}
+          {toggle && (
+            <CreateContent
               updateContents={updateContents}
               setUpdateContents={setUpdateContents}
             />
-          );
-        })}
-        <div
-          className="memory-contents-default"
-          onClick={createContentFromDefault}
-        >
-          <p className="memory-contents-default-title">카테고리 추가하기</p>
-        </div>
-      </div>
+          )}
+
+          <div className="memory-contents">
+            {contents?.map((content: any) => {
+              return (
+                <ContentCard
+                  id={content._id}
+                  key={content._id}
+                  content={content.content}
+                  description={content.description}
+                  time={content.updatedAt}
+                  updateContents={updateContents}
+                  setUpdateContents={setUpdateContents}
+                />
+              );
+            })}
+            <div
+              className="memory-contents-default"
+              onClick={createContentFromDefault}
+            >
+              <p className="memory-contents-default-title">카테고리 추가하기</p>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
