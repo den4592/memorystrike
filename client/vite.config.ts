659a4d0e498a3f5fd436dfaf520d326669149554
den4592 <den4592@naver.com>
2023-06-06 19:34:13 +0900

    model path 변경

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,6 +6,11 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: "./",
+  build: {
+    outDir: "dist",
+  },
+  assetsInclude: ["memory_strike_brain.glb"],
   plugins: [react(), viteTsconfigPaths(), svgr({ exportAsDefault: true })],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "./src") }],
