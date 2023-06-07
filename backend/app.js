97dc2691fee38b8e764de17b3a749b59d15a20ee
den4592 <den4592@naver.com>
2023-06-07 14:08:10 +0900

    vercer file setting

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "http://127.0.0.1:5173", // 접근 권한을 부여하는 도메인
+    origin: "https://memorystrike.com", // 접근 권한을 부여하는 도메인
     credentials: true, // 응답 헤더에 Access-Control-Allow-Credentials 추가
     optionsSuccessStatus: 200, // 응답 상태 200으로 설정
   })
@@ -20,7 +20,7 @@ const statisticRoutes = require("./routes/statistics-routes");
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5173"); // 모든 출처(origin)을 허용
+  res.setHeader("Access-Control-Allow-Origin", "*"); // 모든 출처(origin)을 허용
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
