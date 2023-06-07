e617ab89067ebef78f684654a744da6918d4acbd
den4592 <den4592@naver.com>
2023-06-07 14:38:13 +0900

    vercel deploy test

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "https://memorystrike.com", // 접근 권한을 부여하는 도메인
+    origin: "http://127.0.0.1:5173", // 접근 권한을 부여하는 도메인
     credentials: true, // 응답 헤더에 Access-Control-Allow-Credentials 추가
     optionsSuccessStatus: 200, // 응답 상태 200으로 설정
   })
@@ -20,7 +20,7 @@ const statisticRoutes = require("./routes/statistics-routes");
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*"); // 모든 출처(origin)을 허용
+  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5173"); // 모든 출처(origin)을 허용
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
@@ -63,3 +63,5 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+module.exports = app;
