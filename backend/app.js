1cff059c8b74f7e261644245efd1f66cec9d978b
den4592 <den4592@naver.com>
2023-06-08 16:34:58 +0900

    port 443 -> 8080

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "https://memorystrike.com", // 접근 권한을 부여하는 도메인
+    origin: "http://127.0.0.1:5173", // 접근 권한을 부여하는 도메인
     credentials: true, // 응답 헤더에 Access-Control-Allow-Credentials 추가
     optionsSuccessStatus: 200, // 응답 상태 200으로 설정
   })
@@ -20,7 +20,7 @@ const statisticRoutes = require("./routes/statistics-routes");
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://memorystrike.com"); // 모든 출처(origin)을 허용
+  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5173"); // 모든 출처(origin)을 허용
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
@@ -58,7 +58,7 @@ mongoose
     { autoIndex: false }
   )
   .then(() => {
-    app.listen(443);
+    app.listen(8080);
   })
   .catch((err) => {
     console.log(err);
