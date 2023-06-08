541866428daa6d4c1d2e564746d1f076b684e4f9
den4592 <den4592@naver.com>
2023-06-08 18:26:54 +0900

    다중 origin test

--- backend/app.js
+++ backend/app.js
@@ -20,10 +20,9 @@ const statisticRoutes = require("./routes/statistics-routes");
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "http://127.0.0.1:5173, https://www.memorystrike.com"
-  );
+  // res.setHeader(
+  //   "Access-Control-Allow-Origin", "http://127.0.0.1:5173, https://www.memorystrike.com"
+  // );
   // res.setHeader("Access-Control-Allow-Origin", "https://www.memorystrike.com");
   res.setHeader(
     "Access-Control-Allow-Methods",
