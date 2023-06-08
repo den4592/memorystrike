65b6d72ffb99886030456f81fbdaac3582a369fd
den4592 <den4592@naver.com>
2023-06-08 22:20:38 +0900

    http://localhost:4173 origin 추가

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ app.use(
     origin: [
       "https://www.memorystrike.com",
       "http://127.0.0.1:5173",
-      "http://localhost:4173/",
+      "http://localhost:4173",
     ], // 접근 권한을 부여하는 도메인
     credentials: true, // 응답 헤더에 Access-Control-Allow-Credentials 추가
     optionsSuccessStatus: 200, // 응답 상태 200으로 설정
