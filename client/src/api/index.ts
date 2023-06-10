912d8dcce7782ee563ea92b2f32dd6d723a367f1
den4592 <den4592@naver.com>
2023-06-11 00:02:05 +0900

    .DS_Store removed

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -7,7 +7,6 @@ export const httpClient = axios.create({
   baseURL: import.meta.env.VITE_SERVER_URL,
 });
 
-//axios로 사용하지 않고, httpClientForCredentials로 사용
 export const httpClientForCredentials = axios.create({
   headers: {
     "Access-Control-Allow-Origin": import.meta.env.VITE_SERVER_URL, // 서버 domain
