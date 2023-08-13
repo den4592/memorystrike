f5b2c997a4bb1ccafab326e0d7c85c7dc23e2509
den4592 <den4592@naver.com>
2023-08-13 16:40:36 +0900

    카테고리 카드 수정 시 불필요한 API call 제거

--- client/src/pages/Memory/components/ContentCard/index.tsx
+++ client/src/pages/Memory/components/ContentCard/index.tsx
@@ -58,6 +58,8 @@ const ContentCard = ({
 
   const handleEdit = useCallback(async () => {
     try {
+      setPrevText(contentText);
+      setPrevDesc(descriptionText);
       if (
         enableEdit === true &&
         (prevText !== contentText || prevDesc !== descriptionText)
