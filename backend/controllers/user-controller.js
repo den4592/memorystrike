0e6d4f24627347059480fad7cce390ff7fae5c98
den4592 <den4592@naver.com>
2023-08-16 23:45:01 +0900

    testing domain

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -169,9 +169,7 @@ const login = async (req, res, next) => {
     res.cookie("refreshToken", refreshToken, {
       expiresIn: 30 * 24 * 60 * 60 * 1000,
       httpOnly: true,
-      sameSite: "none", // 도메인 간 쿠키 공유를 위해 필요
-      secure: true, // HTTPS에서만 쿠키 전송 가능
-      domain: "memorystrike.com", // 서브도메인을 포함한 도메인 설정
+      domain: "www.memorystrike.com", // 서브도메인을 포함한 도메인 설정
     }); // 30 days
 
     //setting cookie in cache memory
